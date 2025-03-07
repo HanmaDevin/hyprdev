ffbeaecd313044ea602d863d0922b4ab6699b0f8
HanmaDevin <devinbc14@gmail.com>
2025-03-07 01:08:33 +0100

    added low battery message

---
+++ .config/hypr/battery.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+while true; do
+	BAT=$(ls /sys/class/power_supply |grep BAT |head -n 1)
+	capacity="$(cat /sys/class/power_supply/${BAT}/capacity)"
+	BATSTATUS="$(cat /sys/class/power_supply/${BAT}/status)"
+
+	if [ "$BATSTATUS" == "Discharging" ]; then
+		if [ "$capacity" -le 20 ]; then
+		  notify-send "Battery Warning!" "Battery is at ${capacity}%"
+		else
+		  echo " " > /dev/null
+		fi
+	fi
+done
