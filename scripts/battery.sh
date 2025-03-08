a8f2d93eba6d1b148f257e0a9ad1b4dc372430e5
HanmaDevin <devinbc14@gmail.com>
2025-03-08 11:50:37 +0100

    fixed bug with battery notification

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 while true; do
-	BAT=$(ls /sys/class/power_supply |grep BAT |head -n 1)
+	BAT=$(ls /sys/class/power_supply | grep BAT | head -n 1)
 	capacity="$(cat /sys/class/power_supply/${BAT}/capacity)"
 	BATSTATUS="$(cat /sys/class/power_supply/${BAT}/status)"
 
 	if [ "$BATSTATUS" == "Discharging" ]; then
 		if [ "$capacity" -le 20 ]; then
 		  notify-send "Battery Warning!" "Battery is at ${capacity}%"
-		else
-		  echo " " > /dev/null
+		  exit 9
 		fi
 	fi
 done
