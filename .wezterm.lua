ed14ce0631db41619ceadb0a8836c8a1d47d2ec6
HanmaDevin <devinbc14@gmail.com>
2025-03-21 23:12:33 +0100

    switch to wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+
+-- wayland not supported yet
+config.enable_wayland = false
+
+config.enable_tab_bar = false
+
+config.window_background_opacity = 0.8
+
+config.font_size = 14
+
+-- and finally, return the configuration to wezterm
+return config
